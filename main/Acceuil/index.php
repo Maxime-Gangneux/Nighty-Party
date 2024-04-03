d4ba247b4b0a0f15f067a772d900a7624d092efc
Muller Julien <mullerjulien995@gmail.com>
2024-04-03 15:09:31 +0200

    Création d'organisation de fichier
    Le début

---
+++ main/Acceuil/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nighty Party</title>
+    <link rel="stylesheet" href="css.css">
+</head>
+<body>
+    <div class = "main" >
+        Nichty Partfofh
+    </div > 
+    <?php 
+        // Code PHP
+        echo "Bonjour, monde!";
+    ?>
+</body>
+</html>
