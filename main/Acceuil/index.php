e3b4df700291d998270b1bf2e1285bd2129cf6b4
Gangneux Maxime <gangneuxmaxime@gmail.com>
2024-04-10 15:45:07 +0200

    fin du style de la page d'acceuil

--- main/Acceuil/index.php
+++ main/Acceuil/index.php
@@ -5,36 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nighty Party</title>
     <link rel="stylesheet" href="css.css">
-    <script src = "app.js"></script>
+    <script src="app.js"></script>
 </head>
 <body>
-    <span class = "title_home" >
-        Nighty Party
-    </span >
     <?php
-            include '../nav_barre/nav_barre.php';
+        include '../nav_barre/nav_barre.php';
     ?>
-        <div class ="container_tendance">cvl
-        </div>
-        <div class ="container_tendance">kl
-        </div>
-        <div class ="container_tendance">dl
-        </div>
-        <div class ="container_tendance">cvl
-        </div>
-        <div class ="container_tendance">kl
-        </div>
-        <div class ="container_tendance">dl
-        </div>
-        <div class ="container_tendance">dl
-        </div>
-        <div class ="container_tendance">dl
-        </div>
-        <div class ="container_tendance">dl
-        </div>
-        <div class ="container_tendance">dl
-        </div>
-        <div class ="container_tendance">dl
-        </div>
+
+    <div class="title_home">Nighty Party</div>
+    
+    <div class="container">
+        <div class="container_tendance">cvl</div>
+        <div class="container_tendance">kl</div>
+        <div class="container_tendance">dl</div>
+        <div class="container_tendance">cvl</div>
+        <div class="container_tendance">kl</div>
+        <div class="container_tendance">dl</div>
+    </div>
+
+    <footer>
+        <p>Created and designed by Mulller Julien & Gangneux Maxime</p>
+    </footer>
 </body>
 </html>
