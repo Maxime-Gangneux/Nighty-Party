dc582f69c2c77da5c8c55641ec6b2f2692b5b2cb
Gangneux Maxime <gangneuxmaxime@gmail.com>
2024-05-05 18:23:41 +0200

    soirre

---
+++ main/soiree/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nighty Party</title>
+    <link rel="stylesheet" href="css.css">
+    <script src="app.js"></script>
+</head>
+<body>
+    <?php
+        include '../nav_barre/nav_barre.php';
+    ?>
+    <section>
+        <div class = "image"></div>
+        <div class = "info_container">
+            <div class = "infos_general">
+                <p>titre</p>
+                <p>date</p>
+                <p>description</p>
+            </div>
+            <div class = "contenue">
+                <div class ="personnes"></div>
+                <div class = "boissons"></div>
+            </div>
+        </div>
+
+    </section>
+    <footer>
+        <p>Created and designed by Muller Julien & Gangneux Maxime</p>
+    </footer>
+</body>    
+</html>
