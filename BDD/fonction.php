c3af5cc36ff1a2594f52bb62f70218b6a648b5c6
Gangneux Maxime <gangneuxmaxime@gmail.com>
2024-05-07 14:35:43 +0200

    corriger bb

--- BDD/fonction.php
+++ BDD/fonction.php
@@ -1,16 +1,14 @@
 <?php
+
 if(!isset($_SESSION['connected']) || $_SESSION['connected'] !== true){
     $statu_soiree = $connexion->execute('SELECT statu_soiree FROM soiree');
     if ($statu_soiree > 0){
-        echo"<button name='join_public' class='join_public'>Join the party</button>";
-    }else{
-        echo"<button name='join_private' class='join_private'>Join the party</button>";
+        echo "<button name='join_public' class='join_public'>Join the party</button>";
+    } else {
+        echo "<button name='join_private' class='join_private'>Join the party</button>";
     }  
-    if (isset('join_public') || isset('join_private') ){
-        echo"Vous devez etre connecter pour pouvoir vous inscrire"
+    if (isset($_POST['join_public']) || isset($_POST['join_private']) ){
+        echo "Vous devez être connecté pour pouvoir vous inscrire";
     }
-}else{
-    
 }
-
 ?>
