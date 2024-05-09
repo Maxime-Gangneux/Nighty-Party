7613fbeb40173d8054419400f09a2700128a3872
Muller Julien <mullerjulien995@gmail.com>
2024-05-09 17:14:24 +0200

    a

--- BDD/insert_soiree.php
+++ BDD/insert_soiree.php
@@ -1,6 +1,7 @@
 <?php
 include 'conexion.php';
 $connexion = connecterBaseDonnees();
+session_start();
 if(isset($_POST['submit_button'])){
     $nom_soiree = trim($_POST['nom_soiree']);
     $description_soiree = trim($_POST['description_soiree']);
@@ -16,6 +17,7 @@ if(isset($_POST['submit_button'])){
     $theme_soiree = addslashes(trim($_POST['theme_soiree']));
     $type_soiree = trim($_POST['type_soiree']);
     $statu_soiree = trim($_POST['statu_soiree']);
+    $id_compte = $_SESSION['id_compte'];
 
     // Vérification si tous les champs sont remplis
     if(empty($nom_soiree) || empty($description_soiree) || empty($adresse_soiree) || empty($date_soiree) || empty($heure_min_soiree) || empty($heure_max_soiree) || empty($nb_personne_soiree) || empty($theme_soiree) || empty($type_soiree) || empty($statu_soiree)) {
@@ -26,11 +28,22 @@ if(isset($_POST['submit_button'])){
 
         // Exécution de la requête
         if ($connexion->query($sql) === TRUE) {
-            echo "Données enregistrées avec succès !";
-        } else {
-            echo "Erreur : " . $connexion->error;
-        }
+            // Récupération de l'ID de la soirée nouvellement créée
+            $id_soiree = $connexion->insert_id;
 
+            // Préparation de la requête SQL pour inscrire le compte créateur à la soirée
+            $requete = $connexion->prepare('INSERT INTO invite (id_compte, id_soiree) VALUES (?, ?)');
+            $requete->bind_param("ii", $id_compte, $id_soiree);
+
+            // Exécution de la requête pour inscrire le compte créateur à la soirée
+            if ($requete->execute() === TRUE) {
+                echo "Soirée créée avec succès et compte inscrit à la soirée !";
+            } else {
+                echo "Erreur lors de l'inscription du compte à la soirée : " . $connexion->error;
+            }
+        } else {
+            echo "Erreur lors de la création de la soirée : " . $connexion->error;
+        }
         // Fermeture de la connexion
         $connexion->close();
     }
