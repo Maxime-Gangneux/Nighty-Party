4fc717013b91276807c1330385aa679aebd5e533
Gangneux Maxime <gangneuxmaxime@gmail.com>
2024-05-14 08:52:32 +0200

    oui bb

--- BDD/fonction.php
+++ BDD/fonction.php
@@ -1,110 +1,90 @@
 <?php
-$statu_soiree = $ligne['statu_soiree'];
-$id_soiree = $ligne['id_soiree'];
-
-if(!isset($_SESSION['connected']) || $_SESSION['connected'] !== true){
+if (!isset($_SESSION['connected']) || $_SESSION['connected'] !== true) {
     $result_verif_editeur = 0;
 } else {
     $id_compte = $_SESSION['id_compte'];
 
-<<<<<<< Updated upstream
-    // Requête pour vérifier si l'utilisateur actuel est l'éditeur de la soirée
-    $requete_verif_editeur = $connexion->prepare('SELECT id_editeur FROM invite WHERE id_compte = ? AND id_soiree = ? AND id_editeur = 1');
-    $requete_verif_editeur->bind_param("ii", $id_compte, $id_soiree);
-    $requete_verif_editeur->execute();
-    
-    // Récupérer le résultat de la requête
-    $result_verif_editeur = $requete_verif_editeur->get_result();
+    $requete_verif_editeur = "SELECT EXISTS (
+                                    SELECT 1
+                                    FROM soiree
+                                    INNER JOIN invite ON soiree.id_soiree = invite.id_soiree
+                                    WHERE soiree.id_soiree = 13 AND soiree.id_invite = 2
+                                ) AS is_editeur;";
 
-    // Vérifier s'il y a des résultats
-    if ($result_verif_editeur->num_rows > 0) {
-        // Si oui, l'utilisateur est l'éditeur de la soirée
-        $result_verif_editeur = 1;
-    } else {
-        // Sinon, l'utilisateur n'est pas l'éditeur de la soirée
-        $result_verif_editeur = 0;
-    }
-}
-
-
-if ($result_verif_editeur == 1 && isset($_SESSION['connected'])) {
-=======
-    $requete_verif_editeur = "SELECT id_editeur FROM invite WHERE id_compte = $id_compte AND id_soiree = $id_soiree AND id_editeur = 1";
+    // Exécution de la requête
     $result_verif_editeur = $connexion->query($requete_verif_editeur);
-    $row = $result_verif_editeur->fetch_assoc();
-    $id_editeur = $row['id_editeur'];
-}
 
+    if($result_verif_editeur){
+        $row = $result_verif_editeur->fetch_assoc();
+    }
 
-
-if ($id_editeur == 1 && isset($_SESSION['connected'])) {
->>>>>>> Stashed changes
-    echo"vous ete l'editeur de la soirée";
-}else{
-    if(!isset($_SESSION['connected']) || $_SESSION['connected'] !== true){
-        if ($statu_soiree > 0){
-            echo"
-            <form method='POST'>
-                <button name='join_public' class='join_public'>Join the party</button>
-            <input type='hidden' name='id_soiree' value='{$id_soiree}'>
-            </form>";
-        }else{
-            echo"
-            <form method='POST'>
-                <button name='join_private' class='join_private'>Join the party</button>
-                <input type='hidden' name='id_soiree' value='{$id_soiree}'>
-            </form>";
-        }  
-        if (isset($_POST['join_public']) || isset($_POST['join_private']) ){
-            echo"Vous devez etre connecter pour pouvoir vous inscrire";
-        }
-    }else{
-        $requete_verif = $connexion->prepare('SELECT id_invite FROM invite WHERE id_compte = ? AND id_soiree = ?');
-        $requete_verif->bind_param("ii", $id_compte, $id_soiree);
-        $requete_verif->execute();
-        $result_verif = $requete_verif->get_result();
-        if($result_verif->num_rows > 0) {
-            echo "
-            <form method='POST'>
-                <button name='exit_party'>quitter la soirée</button>
-                <input type='hidden' name='id_soiree' value='{$id_soiree}'>
-            </form>";
-            if (isset($_POST['exit_party'])){
-                $requete_exit = $connexion->prepare('DELETE FROM invite WHERE id_compte = ? AND id_soiree = ? ');
-                $requete_exit->bind_param("ii", $id_compte, $id_soiree);
-                if ($requete_exit->execute()){
-                    echo"<script> window.location.href = window.location.href ;</script>";
-                } else {
-                    echo "Erreur lors de la suppression : " . $requete_suppression->error;
-                }    
-            }
-        }else {
-            if ($statu_soiree < 1){
-                echo"<button name='join_private' class='join_private'>Join the party private</button>";
-            }else{
-                echo"
+    if ($row["is_editeur"] && isset($_SESSION['connected'])) {
+        echo "Vous êtes l'éditeur de la soirée";
+    } else {
+        if (!isset($_SESSION['connected']) || $_SESSION['connected'] !== true) {
+            if ($statu_soiree > 0) {
+                echo "
                 <form method='POST'>
-                    <button name='join_public' class='join_public'>Join the party public</button>
+                    <button name='join_public' class='join_public'>Rejoindre la soirée publique</button>
+                    <input type='hidden' name='id_soiree' value='{$id_soiree}'>
+                </form>";
+            } else {
+                echo "
+                <form method='POST'>
+                    <button name='join_private' class='join_private'>Rejoindre la soirée privée</button>
                     <input type='hidden' name='id_soiree' value='{$id_soiree}'>
                 </form>";
             }
-            if (isset($_POST['join_public'])){
-                $requete = $connexion->prepare('INSERT INTO invite (id_compte, id_soiree, id_editeur) VALUES (?, ?, ?)');
-                $zero = 0;
-                $requete->bind_param("iii", $id_compte, $id_soiree, $zero);
-                $requete->execute();
-
-                if($requete) {
-                    echo"<script> window.location.href = window.location.href ;</script>";
-                } else {
-                    echo "Erreur lors de l'insertion : " . $requete->errorInfo();
-                }
-            if (isset($_POST['join_private'])){
-                echo"vous ne pouvez pas encore ajouter de soirée privée vue que on est des feniasse";
+            if (isset($_POST['join_public']) || isset($_POST['join_private'])) {
+                echo "Vous devez être connecté pour pouvoir vous inscrire";
             }
+        } else {
+            $requete_verif = $connexion->prepare('SELECT id_invite FROM invite WHERE id_compte = ? AND id_soiree = ?');
+            $requete_verif->bind_param("ii", $id_compte, $id_soiree);
+            $requete_verif->execute();
+            $result_verif = $requete_verif->get_result();
+            if ($result_verif->num_rows > 0) {
+                echo "
+                <form method='POST'>
+                    <button name='exit_party'>Quitter la soirée</button>
+                    <input type='hidden' name='id_soiree' value='{$id_soiree}'>
+                </form>";
+                if (isset($_POST['exit_party'])) {
+                    $requete_exit = $connexion->prepare('DELETE FROM invite WHERE id_compte = ? AND id_soiree = ? ');
+                    $requete_exit->bind_param("ii", $id_compte, $id_soiree);
+                    if ($requete_exit->execute()) {
+                        echo "<script>window.location.href = window.location.href;</script>";
+                    } else {
+                        echo "Erreur lors de la suppression : " . $requete_suppression->error;
+                    }
+                }
+            } else {
+                if ($statu_soiree < 1) {
+                    echo "<button name='join_private' class='join_private'>Rejoindre la soirée privée</button>";
+                } else {
+                    echo "
+                    <form method='POST'>
+                        <button name='join_public' class='join_public'>Rejoindre la soirée publique</button>
+                        <input type='hidden' name='id_soiree' value='{$id_soiree}'>
+                    </form>";
+                }
+                if (isset($_POST['join_public'])) {
+                    $requete = $connexion->prepare('INSERT INTO invite (id_compte, id_soiree, id_editeur) VALUES (?, ?, ?)');
+                    $zero = 0;
+                    $requete->bind_param("iii", $id_compte, $id_soiree, $zero);
+                    $requete->execute();
+
+                    if ($requete) {
+                        echo "<script>window.location.href = window.location.href;</script>";
+                    } else {
+                        echo "Erreur lors de l'insertion : " . $requete->errorInfo();
+                    }
+                }
+                if (isset($_POST['join_private'])) {
+                    echo "Vous ne pouvez pas encore ajouter de soirée privée vue que nous sommes des fainéants.";
+                }
             }
         }
     }
 }
-
 ?>
